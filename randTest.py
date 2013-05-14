e36f336344f2860544f6a4436044ef882289e0d8
alex mccarten-gibbs <alex@mccarten-gibbs.com>
2013-05-13 21:36:35 -0500

    simplified and fixed timing

--- randTest.py
+++ randTest.py
@@ -1,89 +1,83 @@
-import random
-from time import clock, sleep
+from random import randint
+from time import process_time
+#from multiprocessing import Pool, cpu_count
 
-#to do: port to python 3, setup multiprocessing
+#to do: figure out multiprocessing timing
 
-'''
+"""
 randTest: Algorithm that tests how many random
 numbers can be produced with the random module
-without repeating. Is implemented by recursive
-recuTest and iterative iterTest functions.
-'''
+without repeating.
+"""
 
 #recursive version
 def recuTest(limit = None, randList = None):
-	#parameter check
-	if (randList == None): randList = []
-	if (limit == None): limit = 100
-	
-	#create random number
-	rand = random.randint(0, limit)
+        #parameter check
+        if (randList == None): randList = []
+        if (limit == None): limit = 100
+        
+        #create random number
+        rand = randint(0, limit)
 
-	#if in list return list length
-	if (rand in randList): return len(randList)
-	#if not in list add and recurse
-	else: 
-		randList.append(rand)
-		return recuTest(limit, randList)
+        #if in list return list length
+        if (rand in randList):
+                return len(randList)
+        #if not in list add and recurse
+        else:
+                randList.append(rand)
+                return recuTest(limit, randList)
 
-#iterative version		
+#iterative version              
 def iterTest(limit = None, randList = None):
-	#parameter check
-	if (randList == None): randList = []
-	if (limit == None): limit = 100
-	
-	#infinite loop
-	while(True):
-		#create random number
-		rand = random.randint(0, limit)
+        #parameter check
+        if (randList == None): randList = []
+        if (limit == None): limit = 100
+        
+        #infinite loop
+        while(True):
+                #create random number
+                rand = randint(0, limit)
 
-		#if in list return list length
-		if (rand in randList): return len(randList)
-		#if not in list add
-		else: randList.append(rand)
-		
+                #if in list return list length
+                if (rand in randList):
+                        return len(randList)
+                #if not in list add
+                else: randList.append(rand)
+
+
 #calls randTest multiple times and collects statistics
-def randStats(randTest, limit=None, sampleSize=None):
-	#parameter check
-	if (limit == None): limit = 100
-	if (sampleSize == None): sampleSize = 100
-	
-	#setup
-	start = clock()
-	stats = []
-	output = "list of tests: "
-	
-	#run tests and collect results
-	map(lambda x: stats.append(randTest(limit)), range(sampleSize))
-	output += reduce(lambda x, y: str(x) + ", " + str(y), stats)
+def randStats(limit=None, sampleSize=None):
+        #parameter check
+        if (limit == None): limit = 100
+        if (sampleSize == None): sampleSize = 10000
+        
+        #setup
+        stats = [limit] * int(sampleSize / 2)
+        #pool = Pool(cpu_count())
 
-	#calculate statistics
-	stop = clock()
-	average = sum(stats) / len(stats)
-	statsMin = min(stats)
-	statsMax = max(stats)
-	time = stop - start
-	
-	#print results
-	print(output)
-	print("Average tests before repeat: " + str(average))
-	print("Shortest tests before repeat: " + str(statsMin))
-	print("Longest tests before repeat: " + str(statsMax))
-	print("Elapsed time: " + str(time))
+        #run tests and time them        
+        
+        iterStart = process_time()
+        iterList = list(map(iterTest, stats))
+        iterTime = process_time() - iterStart
 
-#runs both randTest implementations for comparison
-def benchmark(limit = None, sampleSize = None):
-	#parameter check
-	if (limit == None): limit = 1000
-	if (sampleSize == None): sampleSize = 1000
-	
-	#script
-	print("Iterative version")
-	randStats(iterTest, limit, sampleSize)
-	sleep(3)
-	print("\n\n\n\n")
-	print("Recursive version")
-	randStats(recuTest, limit, sampleSize)
+        recuStart = process_time()
+        recuList = list(map(recuTest, stats))
+        recuTime = process_time() - recuStart
 
-#run
-benchmark()
+        #calculate statistics
+        stats =  iterList + recuList
+        average = sum(stats) / len(stats)
+        statsMin = min(stats)
+        statsMax = max(stats)
+        
+        #print results
+        print("Number of tests: " + str(len(stats)))
+        print("Average tests before repeat: " + str(average))
+        print("Shortest tests before repeat: " + str(statsMin))
+        print("Longest tests before repeat: " + str(statsMax))
+        print("Iterative time: %.9f seconds" % iterTime)
+        print("Recursive time: %.9f seconds" % recuTime)
+        
+#script
+randStats()
